c60ac26edb3a1f313e55acf9e93eede598ae6da0
AlexandrePsq <alexandre-pasquiou@orange.fr>
2020-05-13 10:19:20 +0200

    correct functions

--- GPT2/utils.py
+++ GPT2/utils.py
@@ -1,8 +1,104 @@
-from collections import namedtuple, defaultdict
 import os
+import wget
+import time
+import yaml
+import glob
+import torch
+import random
+import inspect
+import datetime
+import argparse
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+#########################################
+############ Basic functions ############
+#########################################
+
+def check_folder(path):
+    """Create adequate folders if necessary."""
+    try:
+        if not os.path.isdir(path):
+            check_folder(os.path.dirname(path))
+            os.mkdir(path)
+    except:
+        pass
+    
+def read_yaml(yaml_path):
+    """Open and read safely a yaml file."""
+    with open(yaml_path, 'r') as stream:
+        try:
+            parameters = yaml.safe_load(stream)
+        except :
+            print("Couldn't load yaml file: {}.".format(yaml_path))
+            quit()
+    return parameters
+
+def save_yaml(data, yaml_path):
+    """Open and write safely in a yaml file.
+    Arguments:
+        - data: list/dict/str/int/float
+        -yaml_path: str
+    """
+    with open(yaml_path, 'w') as outfile:
+        yaml.dump(data, outfile, default_flow_style=False)
+
+def filter_args(func, d):
+    """ Filter dictionary keys to match the function arguments.
+    Arguments:
+        - func: function
+        - d: dict
+    Returns:
+        - args: dict
+    """
+    keys = inspect.getfullargspec(func).args
+    args = {key: d[key] for key in keys if ((key!='self') and (key in d.keys()))}
+    return args
+
+def get_device(device_number=0, local_rank=-1):
+    """ Get the device to use for computations.
+    """
+    if local_rank == -1:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        n_gpu = torch.cuda.device_count()
+        print('There are %d GPU(s) available.' % torch.cuda.device_count())
+        if torch.cuda.is_available():
+            print('We will use the GPU:', torch.cuda.get_device_name(device_number))
+        else:
+            print('No GPU available, using the CPU instead.')
+    else:
+        torch.cuda.set_device(local_rank)
+        device = torch.device("cuda", local_rank)
+        n_gpu = 1
+        # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+        torch.distributed.init_process_group(backend='nccl')
+    return device
+
+def format_time(elapsed):
+    """
+    Takes a time in seconds and returns a string hh:mm:ss
+    """
+    # Round to the nearest second.
+    elapsed_rounded = int(round((elapsed)))
+    # Format as hh:mm:ss
+    return str(datetime.timedelta(seconds=elapsed_rounded))
+
+def set_seed(value=1111):
+    """ Set all seeds to a given value for reproductibility."""
+    random.seed(value)
+    np.random.seed(value)
+    torch.manual_seed(value)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(value)
 
 
+
+#########################################
+###### Activations related functions ####
+#########################################
 
 
 def match_tokenized_to_untokenized(tokenized_sent, untokenized_sent, connection_character='Ġ'):
@@ -29,14 +125,25 @@ def match_tokenized_to_untokenized(tokenized_sent, untokenized_sent, connection_
         tokenized_sent_index += 1
     return mapping
 
-
-
-def extract_activations_from_tokenized(activation, mapping):
+def extract_activations_from_token_activations(activation, mapping):
     """Take the average activations of the tokens related to a given word."""
     new_activations = []
     for word_index in range(len(mapping.keys())):
         word_activation = []
         word_activation.append([activation[:,index, :] for index in mapping[word_index]])
+        word_activation = np.vstack(word_activation)
+        new_activations.append(np.mean(word_activation, axis=0).reshape(1,-1))
+    return new_activations
+
+def extract_heads_activations_from_token_activations(activation, mapping):
+    """Extract heads activations of each layer for each token.
+    Take the average activations of the tokens related to a given word.
+    activation.shape: [nb_layers, nb_heads, sequence_length, hidden_size/nb_heads]"""
+    new_activations = []
+    #activation = np.swapaxes(activation.squeeze(), 0, 1) # dimension: (nb_tokens, nb_heads)
+    for word_index in range(len(mapping.keys())):
+        word_activation = []
+        word_activation.append([activation[:, :, index, :] for index in mapping[word_index]])
         word_activation = np.vstack(word_activation)
         new_activations.append(np.mean(word_activation, axis=0).reshape(1,-1))
     return new_activations
