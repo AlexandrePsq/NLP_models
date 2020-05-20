0a0a0fb9a515c0ffbc338ca73b25c24fd7f41fe5
AlexandrePsq <alexandre-pasquiou@orange.fr>
2020-05-20 09:58:15 +0200

    add args in instanciation of glove model

--- GLOVE/model.py
+++ GLOVE/model.py
@@ -11,21 +11,21 @@ import numpy as np
 from tokenizer import tokenize
 from modeling_hacked_glove import Glove
 
-from utils import embeddings
+from utils import embeddings, filter_args
 
 
 
 class GloveExtractor(object):
     """Container module for Glove embeddings extraction."""
 
-    def __init__(self, config_path, language, name='', prediction_type='sequential', output_hidden_states=False):
+    def __init__(self, pretrained_glove_model=None ,language='english', prediction_type='sequential', **kwargs):
         super().__init__()
         
-        self.model = Glove()
+        self.model = Glove(pretrained_glove_model, **kwargs)
         self.tokenizer = tokenize
         
         self.language = language
-        self.FEATURE_COUNT = self.model.param['embedding-size']
+        self.FEATURE_COUNT = self.model.param['embedding_size']
         self.name = self.model.__name__()
         self.config = self.model.param
         self.prediction_type = prediction_type
