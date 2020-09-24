117f44578518d8043e7bd7078109ca12710219a2
AlexandrePsq <alexandre-pasquiou@orange.fr>
2020-09-24 10:38:12 +0200

    fix issue in dimension

--- BERT/model.py
+++ BERT/model.py
@@ -246,10 +246,11 @@ class BertExtractor(object):
 
             attention_mask = attention_mask.squeeze(0)
 
-            size = len(tokenized_text) - indexes[index][0]
+            beg = indexes[index][0] + 1 # because of the special token at the beginning
+            end = indexes[index][1] + 1 # because of special token
 
-            inputs_ids = inputs_ids[-size:, :]
-            attention_mask = attention_mask[-size:, :]
+            inputs_ids = inputs_ids[beg:end, :]
+            attention_mask = attention_mask[beg:end, :]
 
             with torch.no_grad():
                 encoded_layers = self.model(inputs_ids, attention_mask=attention_mask, position_ids=position_ids_list[index]) # last_hidden_state, pooler_output, hidden_states, attentions
@@ -262,8 +263,7 @@ class BertExtractor(object):
                 # filtration
                 if self.model.config.output_hidden_states:
                     hidden_states_activations_ = np.vstack([torch.cat([encoded_layers[2][layer][i,len(tokenized_text) - encoded_layers[2][layer].size(0) + i,:].unsqueeze(0) for i in range(encoded_layers[2][layer].size(0))], dim=0).unsqueeze(0).detach().numpy() for layer in range(len(encoded_layers[2]))]) # retrieve all the hidden states (dimension = layer_count * len(tokenized_text) * feature_count)
-                    if indexes[index][0] > 0:
-                        hidden_states_activations_ = np.concatenate([np.zeros((hidden_states_activations_.shape[0], indexes[index][0] , hidden_states_activations_.shape[-1])), hidden_states_activations_], axis=1)
+                    hidden_states_activations_ = np.concatenate([np.zeros((hidden_states_activations_.shape[0], indexes[index][0] + 1 , hidden_states_activations_.shape[-1])), hidden_states_activations_, np.zeros((hidden_states_activations_.shape[0], len(tokenized_text) - indexes[index][1] - 1, hidden_states_activations_.shape[-1]))], axis=1)
                     hidden_states_activations += utils.extract_activations_from_token_activations(hidden_states_activations_, mapping, indexes[index])
                     #cls_activations_, sep_activations_ = utils.extract_activations_from_special_tokens(hidden_states_activations_, mapping)
                     #cls_hidden_states_activations += cls_activations_
